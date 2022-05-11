39fe876c2d692d3f91029fbda120f83bfc9047c3
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-11 14:33:10 -0400

    db.query success

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,7 +9,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        // your mysql username,
+        user: 'root',
+        // your mysql password
+        password: 'C0de4me23*',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+
+// db object using query method and executes a callback with all resulting rows matching query
+db.query('Select * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
