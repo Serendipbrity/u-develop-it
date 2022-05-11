0148ae54bea2fc55507f30805f7d5631498e2ecb
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-05-11 14:58:13 -0400

    delete/create/get candidate

--- server.js
+++ server.js
@@ -24,9 +24,39 @@ const db = mysql.createConnection(
 
 
 // db object using query method and executes a callback with all resulting rows matching query
-db.query('Select * FROM candidates', (err, rows) => {
-    console.log(rows);
-});
+// db.query('Select * FROM candidates', (err, rows) => {
+//     console.log(rows);
+// });
+
+
+// get single candidate
+// db.query(`Select * FROM candidates WHERE id= 10`, (err, rows) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(rows);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(result);
+// });
+
+// create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES(?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
